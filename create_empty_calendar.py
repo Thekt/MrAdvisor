6a7e84e3b512d77a6e3975f9c6d972679f0ce708
thomasJaunet <43280852+thomasJaunet@users.noreply.github.com>
2018-11-18 19:03:09 -0400

    Create create_empty_calendar.py

---
+++ create_empty_calendar.py
@@ -0,0 +1,31 @@
+dict_months = {
+    1:{'month': 'January','days': 31,'code': 'JAN'},
+    2:{'month': 'February','days': 28,'code': 'FEB'},
+    3:{'month': 'March','days': 31, 'code': 'MAR'},
+    4:{'month': 'April','days': 30, 'code': 'APR'},
+    5:{'month': 'May','days': 31, 'code': 'MAY'},
+    6:{'month': 'June','days': 30, 'code': 'JUN'},
+    7:{'month': 'July','days': 31, 'code': 'JUL'},
+    8:{'month': 'August','days': 31, 'code': 'AUG'},
+    9:{'month': 'September','days': 30, 'code': 'SEP'},
+    10:{'month': 'October','days': 31,'code': 'OCT'},
+    11:{'month': 'November','days': 30, 'code': 'NOV'},
+    12:{'month': 'December','days': 31, 'code': 'DEC'}
+}
+
+def create_empty_calendar(month_number: int)->list:
+"""creates the template for the month corresponding to month_number, puts it in a list of dicts. Each row is half an hour, each column is a day of the month."""
+    CALENDAR = []
+    for i in range(48):
+        row = {}
+        if i%2 == 0:
+            hour = str(i//2)+':00'
+        else:
+            hour = str(i//2)+':30'
+        row['hour'] = hour
+        for j in range(1,dict_months[month_number]['days']+1,1):
+             row[dict_months[month_number]['code']+ ' ' + str(j)] = '-'  #will eventually hold event name for that day 
+        CALENDAR.append(row)
+    return(CALENDAR)
+    #filename = dict_months[month_number]['month']+'_Calendar.csv'
+    #calendar_writer(filename, CALENDAR)
