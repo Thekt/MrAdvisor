cae58422e27343f6d2929b783f3b1285b1a00f4a
Swathi Chandrasekaran <swathi@swathis-mbp.home>
2018-12-02 20:52:10 -0500

    Include comments

--- main.py
+++ main.py
@@ -4,36 +4,36 @@ Created on Thu Nov 15 12:52:43 2018
 
 @author: Kevin
 """
-
+#importing calendar from script
 from timetable import Calendar, displayT
-import re
+import re 
 import sys
 
 c = Calendar()
 print("Welcome to MrAdvisor!")
 
 filename = input('Enter the file name, extension included: ')
-if filename == 'quit':
+if filename == 'quit':   #Allow user to quit at any time
        sys.exit()
 
-pattern = r'.csv$'
-match = re.search(pattern,filename)
+pattern = r'.csv$' #Check for csv files
+match = re.search(pattern,filename) 
 while match == None:
    filename = input('Enter a valid filename, e.g example.csv: ')
-   if filename == 'quit':
+   if filename == 'quit': #Allow user to quit at any time
        sys.exit()
    match = re.search(pattern,filename)
 
-month_number = int(input('Enter the month number: '))
-if month_number == 'quit':
+month_number = int(input('Enter the month number: ')) #Accept month number for calendar display
+if month_number == 'quit': #Allow user to quit at any time
        sys.exit()
 while type(month_number) != int and month_number < 1 and month_number > 12:
    month_number = int(input('Please enter a valid month number: '))
-   if month_number == 'quit':
+   if month_number == 'quit': #Allow user to quit at any time
        sys.exit()
 
-# filename = 'calendar2.csv'
-# month_number = 11
-c.load(filename)
-file = c.fill(month_number)
-displayT(file, month_number)
+# filename = 'calendar2.csv'   #Our example calendar
+# month_number = 11 #Our example month input
+c.load(filename) #open file
+file = c.fill(month_number)  #Use prioritisation algorithm to fill calendar
+displayT(file, month_number) #Display final calendar after removing clashes in events
